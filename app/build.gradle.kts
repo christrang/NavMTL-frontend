b20cd7c24df7f81309a0445d89e47e416ee0be50
MarvensChery <e2162809@cmaisonneuve.qc.ca>
2023-09-22 10:06:07 -0400

    Ajout de la carte

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin")
 }
 
 android {
@@ -37,7 +38,8 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation ("com.google.android.gms:play-services-maps:18.1.0")
+    implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
--- app/src/main/java/com/example/frontend/MainActivity.kt
+++ app/src/main/java/com/example/frontend/MainActivity.kt
@@ -1,11 +1,39 @@
 package com.example.frontend
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment?
+        mapFragment!!.getMapAsync(this)
+    }
+
+    // Update the map configuration at runtime.
+    override fun onMapReady(googleMap: GoogleMap) {
+        // Set the map coordinates to Montreal, Canada.
+        val montreal = LatLng(45.5017, -73.5673)
+        // Set the map type to Hybrid.
+        googleMap.mapType = GoogleMap.MAP_TYPE_HYBRID
+        // Add a marker on the map coordinates.
+        googleMap.addMarker(
+            MarkerOptions()
+                .position(montreal)
+                .title("Montreal")
+        )
+        // Move the camera to the marker and zoom in.
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(montreal, 11f))
+        // Display traffic.
+        googleMap.isTrafficEnabled = false
     }
 }
