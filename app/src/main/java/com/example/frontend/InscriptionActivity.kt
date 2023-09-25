b155c6636bf2ea1d8f773a8c7f47163ad0c8843e
MarvensChery <e2162809@cmaisonneuve.qc.ca>
2023-09-25 10:14:28 -0400

    Ajout inscription

--- app/src/main/java/com/example/frontend/InscriptionActivity.kt
+++ app/src/main/java/com/example/frontend/InscriptionActivity.kt
@@ -8,21 +8,29 @@ import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 
 
-class LoginActivity : AppCompatActivity() {
+class InscriptionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_connexion)
+        setContentView(R.layout.activity_inscription)
 
         val repository = Repository(application)
         val button = findViewById<Button>(R.id.button)
         val email = findViewById<EditText>(R.id.inputEmail)
         val password = findViewById<EditText>(R.id.inputMdp)
+        val nom = findViewById<EditText>(R.id.inputNom)
+        val prenom = findViewById<EditText>(R.id.inputPrenom)
+        val number = findViewById<EditText>(R.id.inputPhone)
 
-       button.setOnClickListener {
-             val mail = email.text.toString()
-             val mdp  = password.text.toString()
-            Log.e("LoginActivity", "Token")
-            repository.login(mail, mdp) { response -> // ne marche pas encore
+
+        button.setOnClickListener {
+            val mail = email.text.toString()
+            val mdp  = password.text.toString()
+            val nom1  = nom.text.toString()
+            val prenom1  = prenom.text.toString()
+            val number1  = number.text.toString()
+
+            Log.e("InscriptionActivity", "Token")
+            repository.inscription(nom1, prenom1,mail,mdp,number1) { response -> // ne marche pas encore
                 if (response) {
                     Log.e("LoginActivity", "Token")
                     // Gérez ici la suite de l'opération après une connexion réussie
@@ -37,4 +45,3 @@ class LoginActivity : AppCompatActivity() {
         }
     }
 }
-
